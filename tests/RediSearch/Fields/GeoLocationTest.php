6b9a7d4a408d132e24cf1a2811ed8a90560360f6
Ethan Hann <ethanhann@gmail.com>
2017-08-29 08:25:45 -0400

    Add field tests

---
+++ tests/RediSearch/Fields/GeoLocationTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ehann\Tests\RediSearch;
+
+use Ehann\RediSearch\Fields\GeoLocation;
+use PHPUnit\Framework\TestCase;
+
+class GeoLocationTest extends TestCase
+{
+    public function testShouldGetStringValueOfGeoLocation()
+    {
+        $expected = '50.9741 20.1415';
+
+        $actual = (string)(new GeoLocation(50.9741, 20.1415));
+
+        $this->assertEquals($expected, $actual);
+    }
+}
---
+++ tests/RediSearch/Fields/TextFieldTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Ehann\Tests\RediSearch;
+
+use Ehann\RediSearch\Fields\TextField;
+use PHPUnit\Framework\TestCase;
+
+class TextFieldTest extends TestCase
+{
+    /** @var TextField */
+    private $subject;
+    /** @var string */
+    private $fieldName = 'MyTextField';
+    /** @var string */
+    private $fieldType = 'TEXT';
+    /** @var string */
+    private $weightKeyword = 'WEIGHT';
+    /** @var float */
+    private $defaultWeight = 1.0;
+
+    public function setUp()
+    {
+        $this->subject = new TextField($this->fieldName);
+    }
+
+    public function testShouldGetCorrectType()
+    {
+        $type = $this->subject->getType();
+
+        $this->assertEquals($this->fieldType, $type);
+    }
+
+    public function testShouldGetWeight()
+    {
+        $weight = $this->subject->getWeight();
+
+        $this->assertEquals($this->defaultWeight, $weight);
+    }
+
+    public function testShouldSetWeight()
+    {
+        $expected = 243.0;
+
+        $weight = $this->subject
+            ->setWeight($expected)
+            ->getWeight();
+
+        $this->assertEquals($expected, $weight);
+    }
+
+    public function testShouldGetTypeDefinition()
+    {
+        $typeDefinition = $this->subject->getTypeDefinition();
+
+        $this->assertEquals($this->fieldName, $typeDefinition[0]);
+        $this->assertEquals($this->fieldType, $typeDefinition[1]);
+        $this->assertEquals($this->weightKeyword, $typeDefinition[2]);
+        $this->assertEquals($this->defaultWeight, $typeDefinition[3]);
+    }
+}
