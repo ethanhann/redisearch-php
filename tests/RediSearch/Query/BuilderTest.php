62ab7a63bef5d17bc800666087f6a0bd84bd3b41
Ethan Hann <ethanhann@gmail.com>
2017-08-30 15:59:57 -0400

    Resolve a few issues in query builder with LIMIT and SLOP

--- tests/RediSearch/Query/BuilderTest.php
+++ tests/RediSearch/Query/BuilderTest.php
@@ -13,6 +13,7 @@ class BuilderTest extends AbstractTestCase
     private $subject;
     private $expectedResult1;
     private $expectedResult2;
+    private $expectedResult3;
     public function setUp()
     {
         $this->indexName = 'QueryBuilderTest';
@@ -40,6 +41,14 @@ class BuilderTest extends AbstractTestCase
             'location' => new GeoLocation(50.9190500, 4.0504100),
         ];
         $index->add($this->expectedResult2);
+        $this->expectedResult3 = [
+            'title' => 'How to be awesome, part 2, section 13, appendix A',
+            'author' => 'Jack',
+            'price' => 18.95,
+            'stock' => 11,
+            'location' => new GeoLocation(10.9190500, 52.0504100),
+        ];
+        $index->add($this->expectedResult3);
         $this->subject = (new Builder($this->redisClient, $this->indexName));
     }
 
@@ -50,14 +59,14 @@ class BuilderTest extends AbstractTestCase
 
     public function testSearch()
     {
-        $result = $this->subject->search('awesome');
+        $result = $this->subject->search('Shoes');
 
         $this->assertTrue($result->getCount() === 1);
     }
 
     public function testSearchWithScores()
     {
-        $result = $this->subject->withScores()->search('awesome');
+        $result = $this->subject->withScores()->search('Shoes');
 
         $this->assertTrue($result->getCount() === 1);
         $this->assertTrue(property_exists($result->getDocuments()[0], 'score'));
@@ -65,7 +74,7 @@ class BuilderTest extends AbstractTestCase
 
     public function testSearchWithPayloads()
     {
-        $result = $this->subject->withPayloads()->search('awesome');
+        $result = $this->subject->withPayloads()->search('Shoes');
 
         $this->assertEquals(1, $result->getCount());
         $this->assertTrue(property_exists($result->getDocuments()[0], 'payload'));
@@ -95,5 +104,37 @@ class BuilderTest extends AbstractTestCase
 
         $this->assertEquals($expectedCount, $result->getCount());
         $this->assertEquals($this->expectedResult2['author'], $result->getDocuments()[0]->author);
+    }
+
+    public function testLimitSearch()
+    {
+        $expectedCount = 1;
+
+        $result = $this->subject->limit(0, $expectedCount)->search('How');
+
+        $this->assertCount($expectedCount, $result->getDocuments());
+    }
+
+    public function testSearchWithNoContent()
+    {
+        $result = $this->subject->noContent()->search('How');
+
+        $this->assertFalse(property_exists($result->getDocuments()[0], 'title'));
+        $this->assertFalse(property_exists($result->getDocuments()[1], 'title'));
+    }
+
+    public function testSearchWithDefaultSlop()
+    {
+        $result = $this->subject->slop(0)->search('How appendix');
+
+        var_dump($result);
+        $this->assertCount(0, $result->getDocuments());
+    }
+
+    public function testSearchWithNonDefaultSlop()
+    {
+        $result = $this->subject->slop(10)->search('How awesome');
+
+        $this->assertCount(2, $result->getDocuments());
     }
 }
