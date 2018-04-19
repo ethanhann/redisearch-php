8968657a1f12e07dd5a4a15645dfacbc3137ecb3
michael-grunder <michael.grunder@gmail.com>
2018-04-18 19:10:45 -0700

    Support NOINDEX fields

--- src/Fields/GeoField.php
+++ src/Fields/GeoField.php
@@ -4,8 +4,20 @@ namespace Ehann\RediSearch\Fields;
 
 class GeoField extends AbstractField
 {
+    use Noindex;
+
     public function getType(): string
     {
         return 'GEO';
+    }
+
+    public function getTypeDefinition(): array
+    {
+        $properties = parent::getTypeDefinition();
+        if ($this->isNoindex()) {
+            $properties[] = 'NOINDEX';
+        }
+
+        return $properties;
     }
 }
