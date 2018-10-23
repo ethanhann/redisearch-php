7293fb24e0bd7cf74e73de8630941b823d881e74
Philipp Waldhauer <ich@philippwaldhauer.de>
2018-10-23 22:55:04 +0200

    Adding possibility to add aliases to reducers

--- src/Aggregate/Reducers/AbstractFieldNameReducer.php
+++ src/Aggregate/Reducers/AbstractFieldNameReducer.php
@@ -7,14 +7,24 @@ use Ehann\RediSearch\CanBecomeArrayInterface;
 abstract class AbstractFieldNameReducer implements CanBecomeArrayInterface
 {
     public $fieldName;
+    public $alias;
 
-    public function __construct(string $fieldName)
+    public function __construct(string $fieldName, string $alias = '')
     {
         $this->fieldName = $fieldName;
+        $this->alias = $alias;
     }
 
     public function toArray(): array
     {
         return [];
+    }
+
+    protected function addAlias($params) {
+        if(empty($this->alias)) {
+            return $params;
+        }
+
+        return array_merge($params, ['AS', $this->alias]);
     }
 }
