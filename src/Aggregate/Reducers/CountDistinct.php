00abbcc08ffe25c762994008e6dbf85a497b4056
Philipp Waldhauer <ich@philippwaldhauer.de>
2018-11-16 02:10:14 +0100

    Adding possibility to add aliases to reducers (#38)

--- src/Aggregate/Reducers/CountDistinct.php
+++ src/Aggregate/Reducers/CountDistinct.php
@@ -6,6 +6,6 @@ class CountDistinct extends AbstractFieldNameReducer
 {
     public function toArray(): array
     {
-        return ['REDUCE', 'COUNT_DISTINCT', '1', $this->fieldName];
+        return $this->addAlias(['REDUCE', 'COUNT_DISTINCT', '1', $this->fieldName]);
     }
 }
