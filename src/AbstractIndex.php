93fe7f82f996299fa5e3c27d4f3d6e912f430f9f
Marcin G <190209+marcing@users.noreply.github.com>
2026-03-08 15:00:06 +0100

    Fix PHP 8.4+ compatibility: implicitly nullable params, dynamic properties (#87)

--- src/AbstractIndex.php
+++ src/AbstractIndex.php
@@ -9,7 +9,7 @@ abstract class AbstractIndex extends AbstractRediSearchClientAdapter
     /** @var string */
     protected $indexName;
 
-    public function __construct(RedisRawClientInterface $redisClient = null, string $indexName = '')
+    public function __construct(?RedisRawClientInterface $redisClient = null, string $indexName = '')
     {
         parent::__construct($redisClient);
         $this->indexName = $indexName;
--- src/Exceptions/UnknownRediSearchCommandException.php
+++ src/Exceptions/UnknownRediSearchCommandException.php
@@ -6,7 +6,7 @@ use Exception;
 
 class UnknownRediSearchCommandException extends Exception
 {
-    public function __construct($message = '', $code = 0, Exception $previous = null)
+    public function __construct($message = '', $code = 0, ?Exception $previous = null)
     {
         parent::__construct(
             trim("Unknown RediSearch command. Are you sure the RediSearch module is enabled in Redis? $message"),
